5a177ed73f09aa3898f5ac875bdeca357231b278
matildecosta <matilde.a.costa@ua.pt>
2023-03-27 16:43:02 +0100

    lets move to wonindows

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Button, Container} from 'react-bootstrap';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+// import {Routes, Route, useNavigate} from 'react-router-dom';
 import './App.css';
 
 let App = () => {
@@ -8,11 +8,10 @@ let App = () => {
     <Container fluid>
       <Container className='text-center'>
         <h1 className='header'>DocTalk</h1>
-        <Button variant='info' classname="mt-3" onClick={() =>window.location.href='http://localhost:5000/'}>Sign In!</Button>
+        <Button variant='info' classname="mt-3" onClick={() => window.location.href='http://localhost:5000/'}>Sign In!</Button>
       </Container>
     </Container>
   );
 }
-
 
 export default App;
